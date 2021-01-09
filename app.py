41235553f8174d8d7fca86e00ebd80398ef87045
Rich Jiang <rich.jiang2@gmail.com>
2021-01-09 17:30:20 -0500

    added js

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 from boggle import Boggle
-from flask import redirect, Flask, request, render_template, session, make_response
+from flask import redirect, Flask, request, render_template
+from flask import session, make_response, flash
 boggle_game = Boggle()
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = "never-tell!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+board = Boggle()
+layout = board.make_board()
+
+@app.route("/")
+def inits():
+    session['layout'] = layout
+    return redirect("/board")
+
 @app.route("/board")
 def show_board():
-    board = Boggle()
-    layout = board.make_board()
-    return render_template("board.html", board = layout, enum = enumerate)
+    
+    return render_template("board.html", 
+        board = layout, enum = enumerate)
+
+# @app.route("/guess")
+# def check_guess():
+#     word = request.form['word_guess']
+#     if word.lower() in board.words:
+#         print('hello')
+#     else:
+#         flash("That is not a word in the word list")
+
+#     return redirect("/board")
---
+++ static/board_script.js
@@ -0,0 +1,6 @@
+$("#guess_form").submit(async function(e){
+    
+    console.log("testing js code");
+    alert("testing js code")
+    e.preventDefault();
+});
