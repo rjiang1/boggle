89a3d543daefb265a01481ffe7732d03f8491239
Rich Jiang <rich.jiang2@gmail.com>
2021-01-10 14:32:31 -0500

    resets input field on submit

--- static/board_script.js
+++ static/board_script.js
@@ -1,6 +1,12 @@
+// const { default: axios } = require("axios");
+
 $("#guess_form").submit(async function(e){
     
-    console.log("testing js code");
-    alert("testing js code")
+    // console.log("testing js code");
+    // alert("testing js code")
     e.preventDefault();
+    input_val = $('#word_input').val();
+    console.log(input_val);
+    this.reset();
+    // response = axios.get();
 });
